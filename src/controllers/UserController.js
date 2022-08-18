144feb4a49a4253d51297a58ec63760be832a06c
dchueri <diegochueri@gmail.com>
2022-08-18 09:07:14 -0400

    feat(me): added me route for return user profile

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,9 +6,19 @@ const userRepository = new UserRepository(db.Users);
 
 class UserController {
   static async getUser(req, res) {
+    const { userId } = req.body;
+    const attributesForReturnOfSearch = [
+      "id",
+      "name",
+      "email",
+      "favorites",
+      "historic",
+    ];
     try {
-      const { id } = req.params;
-      const me = await UserRepository.findOneById(id);
+      const me = await userRepository.findOneById(
+        userId,
+        attributesForReturnOfSearch
+      );
       return res.status(200).json(me);
     } catch (e) {
       return res.status(400).json(e.message);
@@ -18,32 +28,32 @@ class UserController {
   static async createUser(req, res) {
     try {
       const newUser = req.body;
-      const newUserCreated = await UserRepository.create(newUser);
+      const newUserCreated = await userRepository.create(newUser);
       return res.status(200).json(newUserCreated);
     } catch (e) {
       return res.status(400).json(e.message);
     }
   }
 
-  static async addFavoriteWord(req,res) {
+  static async addFavoriteWord(req, res) {
     try {
-      const {word} = req.params;
-      const {userId} = req.body;
-      await UserRepository.addFavoriteWord(word,userId);
+      const { word } = req.params;
+      const { userId } = req.body;
+      await userRepository.addFavoriteWord(word, userId);
       return res.status(204).json();
     } catch (e) {
       return res.status(400).json(e.message);
     }
   }
 
-  static async removeFavoriteWord(req,res) {
+  static async removeFavoriteWord(req, res) {
     try {
-      const {word} = req.params;
-      const {userId} = req.body;
-      await UserRepository.removeFavoriteWord(word,userId);
+      const { word } = req.params;
+      const { userId } = req.body;
+      await userRepository.removeFavoriteWord(word, userId);
       return res.status(204).json();
     } catch (e) {
-      return res.status(400).json({message: e.message});
+      return res.status(400).json({ message: e.message });
     }
   }
 }
--- src/repositories/Repository.js
+++ src/repositories/Repository.js
@@ -1,17 +1,18 @@
 class Repository {
-    constructor(model) {
-        this.model = model
-    }
+  constructor(model) {
+    this.model = model;
+  }
 
-    async create(entity) {
-        return this.model.create(entity);
-    }
+  async create(entity) {
+    return this.model.create(entity);
+  }
 
-    async findOneById(id) {
-        return this.model.findOne({ where: { id: id } })
-    }
-
-
+  async findOneById(id, attributes) {
+    return this.model.findOne({
+      where: { id: id },
+      attributes: attributes
+    });
+  }
 }
 
-module.exports = Repository
+module.exports = Repository;
